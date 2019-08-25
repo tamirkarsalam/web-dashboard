bd37b49811767f46784e03b4066bf62d0bd24c14
arezooalipanah <arzalipanah@gmail.com>
2019-08-26 04:02:55 +0430

    edit pages

--- web/js/custom.js
+++ web/js/custom.js
@@ -51,6 +51,28 @@ $(document).ready(function() {
         $(".sea-drop-2").fadeOut(50);
     });
 
+    // tree view init 
+    var data = [
+        {
+            name: 'node1',
+            children: [
+                { name: 'child1' },
+                { name: 'child2' }
+            ]
+        },
+        {
+            name: 'node2',
+            children: [
+                { name: 'child3' }
+            ]
+        }
+    ];
+    function tvFunc () {
+        $('#tree1').tree({
+            data: data
+        });
+    };
+
     //ADMIN LEFT MOBILE MENU OPEN
     $(".atab-menu").on('click', function() {
         $(".sb2-1").css("left", "0");
